f013cf041b44a2998bf61c57f24aa852b3593666
MedMath <medmathandit@gmail.com>
2019-02-01 23:31:31 -0500

    MASTER

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -58,4 +58,13 @@ public class LoginTests {
 
 
     }
+
+    @Test
+    public void logOutTest2MASTER() {
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+
+
+    }
+
 }
